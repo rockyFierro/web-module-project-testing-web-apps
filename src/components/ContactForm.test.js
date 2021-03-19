230610cf9dfe93a05797fec43c136e145a9cc649
rockyFierro <finnslighthouse@gmail.com>
2021-03-18 23:13:12 -0700

    unfinished, coming back later

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {findAllByTestId, getByLabelText, getByTestId, render, screen, waitFor, waitForElement } from '@testing-library/react';
+import {findAllByTestId, getAllByTestId, getByLabelText, getByTestId, render, screen, wait, waitFor, waitForElement } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
+import DisplayComponent from './DisplayComponent';
 
 test('renders without errors', ()=>{
     render(<ContactForm/>)
@@ -48,15 +49,38 @@ test('renders ONE error message if user enters a valid first name and last name 
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    render(<ContactForm/>)
+    const {getByLabelText, getByTestId} = render(<ContactForm/>)
+    const email = getByLabelText(/email/i)
+    userEvent.type(email, "emailaddress@netnet")
+    const invalidError =  await waitFor( ()=> getByTestId("error"))
+    expect(invalidError).toBeInTheDocument()
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    render(<ContactForm/>)
+    const {getByLabelText,getByTestId} = render(<ContactForm/>)
+    const firstName = getByLabelText(/first name/i)
+    const email = getByLabelText(/email/i)
+    userEvent.type(email, "Fierro@risktofall.com")
+    userEvent.type(firstName, "Albert")
+    const submit = getByTestId("submit")
+    userEvent.click(submit)
+    const err = await waitFor(()=> getByTestId("error"))
+    expect(err).toBeInTheDocument()
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    render(<ContactForm/>)
+    const {getByLabelText,getByTestId} = render(<ContactForm/>)
+    const firstName = getByLabelText(/first name/i)
+    const email = getByLabelText(/email/i)
+    const lastName = getByLabelText(/last name/i)
+    userEvent.type(email, "Fierro@risktofall.com")
+    userEvent.type(firstName, "Albert")
+    userEvent.type(lastName, "Fierro")
+    const submit = getByTestId("submit")
+    const firstnameDisplay = await waitFor(() => getByTestId("firstnameDisplay"))
+    expect(firstnameDisplay).not.toBeInTheDocument()
+    userEvent.click(submit)
+    expect(firstnameDisplay).toBeInTheDocument()
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
